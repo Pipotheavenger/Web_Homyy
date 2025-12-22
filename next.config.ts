972190e708419127474546021cc1f130d6390566
Pipotheavenger <sebastianfelipevalencia@hotmail.com>
2025-12-22 00:09:34 -0500

    Perfil y sistemas de reseñas

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
