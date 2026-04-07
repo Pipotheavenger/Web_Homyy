94807b8dec5e4a61feeb9b42ac474abba08b879a
nicolasjaramillocely99 <nj.jaramillo@uniandes.edu.co>
2026-04-07 00:44:33 -0500

    fix: Update template for reject the worker

--- src/lib/utils/notificationHelpers.ts
+++ src/lib/utils/notificationHelpers.ts
@@ -378,7 +378,7 @@ export const notifyApplicationRejected = async (
       service_id: serviceId,
       service_title: serviceTitle,
       whatsapp_template: {
-        name: 'postulacion_rechazada',
+        name: 'aplicacion_rechazada',
         parameters: ['{{name}}', serviceTitle],
       },
     },
